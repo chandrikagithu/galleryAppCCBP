375d951354e4efca469d22b1310451180b0158c7
chandrikagithu <chandrikakadam98@gmail.com>
2023-03-29 11:06:37 +0000

    first-commit

---
+++ src/components/ThumbnailItem/index.js
@@ -0,0 +1,22 @@
+// Write your code here.
+import './index.css'
+
+const ThumbnailItem = props => {
+  const {eachList, changeImage, isActive} = props
+  const {id, thumbnailUrl, thumbnailAltText} = eachList
+
+  const activeButton = isActive ? 'active-button' : 'opacity-button'
+
+  const onchangeImage = () => {
+    changeImage(id)
+  }
+
+  return (
+    <li>
+      <button type="button" className={activeButton} onClick={onchangeImage}>
+        <img src={thumbnailUrl} alt={thumbnailAltText} className="tumb-image" />
+      </button>
+    </li>
+  )
+}
+export default ThumbnailItem
